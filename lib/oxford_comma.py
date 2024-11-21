3601a9d6ebe9c63067d457b508e8ae310926aa03
Al Cerdan Lico <alcerdan@yahoo.com>
2024-11-21 10:32:52 -0800

    complete python oxford comma lab

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 2:
+        return ' and '.join(items)
+    elif len(items) > 2:
+        prev_items = ', '.join(items[:-1])
+        return prev_items + ', and ' + items[-1]
+    else:
+        return items[0]
+
